b4d560ff6397784d25328753d948db35681a8139
John Oram <john@sendwithus.com>
2017-01-20 17:35:05 -0800

    basic POC

---
+++ static/base.js
@@ -0,0 +1,11 @@
+var options = {
+    cell_height: 80,
+    vertical_margin: 10
+};
+
+$(document).ready(function(){
+    $('.grid-stack').gridstack(options);
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
