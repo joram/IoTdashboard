532c72414df787c489e333eec12ecbb05da6b29e
John Oram <john.c.oram@gmail.com>
2017-03-05 11:17:03 -0800

    Update server.py

--- server.py
+++ server.py
@@ -15,5 +15,5 @@ app.register_blueprint(auth_views)
 app.register_blueprint(dashboard_views)
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 5000)
+    port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port)
